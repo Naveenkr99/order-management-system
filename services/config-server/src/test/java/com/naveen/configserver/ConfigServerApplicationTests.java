0a98ea8f43b703c95362e951eaed7750183b3f61
naveen <knaveenkr500@gmail.com>
2026-03-02 00:03:56 +0530

    created config-server

---
+++ services/config-server/src/test/java/com/naveen/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
